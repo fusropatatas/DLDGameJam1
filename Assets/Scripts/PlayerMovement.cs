268f8e5472cde43cf1ce55be87f5d86935f19096
Jo <joei.yucoco@obf.ateneo.edu>
2024-06-05 17:50:36 +0800

    Player can move

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+// Followed tutorial: https://youtu.be/whzomFgjT50?si=o4Prgv2zUEDy1Vb8
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
