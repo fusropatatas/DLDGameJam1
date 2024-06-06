7cb4c2e72138ab7990c4208acce908351e99064d
Migs Atienza <160925292+SpokBlok@users.noreply.github.com>
2024-06-06 14:02:39 +0800

    Lever, Door scripts added

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Trigger()
+    {
+        if (GetComponent<BoxCollider2D>().enabled)
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else
+        {
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
--- Assets/LeverScript.cs
+++ Assets/LeverScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class LeverScript : MonoBehaviour
 {
+    public bool isLeverOn;
+    public GameObject door1;
+    public GameObject door2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,19 @@ public class LeverScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            //door1.trigger code
+            //door2.trigger code
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+
     }
 }
