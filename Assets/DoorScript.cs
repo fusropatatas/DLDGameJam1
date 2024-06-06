eee8fc2929fda9d2c256b721f475086e21943bbd
Jo <joei.yucoco@obf.ateneo.edu>
2024-06-06 15:14:59 +0800

    implemented lever and door animations

--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
+    //public Animator animator;
+    bool isDoorOpen = false;
+    public GameObject go;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Testing, delete later
+        if (Input.GetKeyDown(KeyCode.Q)){
+            isDoorOpen = !isDoorOpen;
+            go.GetComponentInChildren<SpriteRenderer>().enabled = isDoorOpen;
+        }
     }
 
     public void Trigger()
--- Assets/LeverScript.cs
+++ Assets/LeverScript.cs
@@ -7,16 +7,23 @@ public class LeverScript : MonoBehaviour
     public bool isLeverOn;
     public GameObject door1;
     public GameObject door2;
+    public Animator animator;
+    //int toggle = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Testing, delete later
+        if (Input.GetKeyDown(KeyCode.E)){
+            isLeverOn = !isLeverOn;
+            animator.SetBool("ActualToggle", isLeverOn);
+        }
         
     }
 
